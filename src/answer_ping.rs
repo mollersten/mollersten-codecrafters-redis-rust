08c26db3940f53809faea46ae248de05bd156681
Isak Möllersten <isak@kth.se>
2024-11-10 18:59:21 +0100

    test

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -1,6 +1,5 @@
 use std::{
-    net::TcpStream,
-    io::{Write, Read},
+    io::{Read, Write}, net::TcpStream, thread
 };
 
 pub fn pong(mut stream: TcpStream) {
@@ -8,9 +7,15 @@ pub fn pong(mut stream: TcpStream) {
         let mut buf = [0; 512];
         let buf_count = stream.read(&mut buf).expect("Couldn't read");
 
+        let mut stream2 = stream.try_clone().unwrap();
+
         if buf_count == 0 {
             break;
         }
+
+        thread::spawn(move||{
+            stream2.write_all(b"PONG\r\n").unwrap();
+        });
         stream.write_all(b"+PONG\r\n").expect("Couldn't write");
     }
 }
