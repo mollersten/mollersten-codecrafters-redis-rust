fa4438569fb339122b727343eb575229983bceb6
Isak Möllersten <isak@kth.se>
2024-11-10 21:24:53 +0100

    test

---
+++ src/parser.rs
@@ -0,0 +1,13 @@
+pub async fn parse_bytes(buf: &[u8]) -> Vec<&str> {
+    let message = std::str::from_utf8(buf).expect("Couldn't get message");
+
+    let split_message = message.split("\r\n");
+
+    let filtered_message: Vec<&str> = split_message.into_iter().filter(|elem| elem.is_ascii()).collect();
+    
+    if filtered_message.len() > 1 {
+        filtered_message[1..].to_vec()
+    } else {
+        Vec::new()
+    }
+}
