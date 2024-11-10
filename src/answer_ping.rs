e92cf2f0da23457c2d0589e15b2e027697598413
Isak Möllersten <isak@kth.se>
2024-11-10 17:57:04 +0100

    Test

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -1,8 +1,14 @@
 use std::{
     net::TcpStream,
-    io::Write,
+    io::{Write, Read},
 };
 
 pub fn pong(mut stream: TcpStream) {
-    stream.write_all(b"+PONG\r\n").expect("Couldn't write");
+    let mut content: String = String::new();
+
+    stream.read_to_string(&mut content).expect("Couldn't read message");
+
+    if content == "PING" {
+        stream.write_all(b"+PONG\r\n").expect("Couldn't write");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     io::Write,
     net::TcpListener,
 };
-use redis_starter_rust::answer_ping::{self, *};
+use redis_starter_rust::answer_ping::*;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,7 +14,7 @@ fn main() {
      for stream in listener.incoming() {
          match stream {
              Ok(stream) => {
-                 answer_ping::pong(stream);
+                 pong(stream);
              }
              Err(e) => {
                  println!("error: {}", e);
