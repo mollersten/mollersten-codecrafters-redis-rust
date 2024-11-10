0166a5495e778d3f7816b15d783d3cd0d68ae490
Isak Möllersten <isak@kth.se>
2024-11-10 18:01:21 +0100

    Test

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -9,6 +9,7 @@ pub fn pong(mut stream: TcpStream) {
 
     stream.read_to_string(&mut content).expect("Couldn't read message");
 
+    dbg!(&content);
     if content == "PING" {
         stream.write_all(b"+PONG\r\n").expect("Couldn't write");
     }
