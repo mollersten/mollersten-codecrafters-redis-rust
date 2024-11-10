9d0dd9d85e3d12ea2202181605fdd2e8d4485fca
Isak Möllersten <isak@kth.se>
2024-11-10 19:44:43 +0100

    test

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -8,23 +8,15 @@ pub fn pong(mut stream: TcpStream) {
         let mut buf = [0; 512];
         let buf_count = stream.read(&mut buf).expect("Couldn't read");
 
+        let mut stream2 = stream.try_clone().unwrap();
 
         if buf_count == 0 {
             break;
         }
 
-        let mut cont = String::with_capacity(512);
-        for byte in buf.bytes() {
-            cont.push_str(&byte.unwrap().to_string());
-            if cont.contains("PING") {
-                let mut stream2 = stream.try_clone().unwrap();
-                thread::spawn(move||{
-                    stream2.write_all(b"PONG\r\n").expect("Error writing...");
-                });
-                cont = String::with_capacity(512);
-            }
-        }
-
+        thread::spawn(move||{
+            stream2.write_all(b"PONG\r\n").expect("Error writing...");
+        });
 
         stream.write_all(b"+PONG\r\n").expect("Error writing...");
     }
