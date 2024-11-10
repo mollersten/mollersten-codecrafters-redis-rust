408296ccd79cd8ce7baf652d9a60cb389c773121
Isak Möllersten <isak@kth.se>
2024-11-10 21:23:13 +0100

    test

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -1,20 +1,23 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-
-
+use crate::parser::*;
 pub async fn pong(mut stream: TcpStream) {
-
         tokio::spawn(async move {
             let mut buf = [0; 512];
             loop {
-                
                 let buf_count = stream.read(&mut buf).await.expect("Error reading...");
                 if buf_count == 0 {
                     break;
                 }
-                stream.write_all(b"+PONG\r\n").await.expect("Error writing...");
+                let message = parse_bytes(&buf).await;
+
+                match message[0] {
+                    "PING" => stream.write_all(b"+PONG\r\n").await.expect("Error writing..."),
+                    "ECHO" if message.len() > 1 => {
+
+                    }, 
+                    _ => {},
+                };
             }
-
         });
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod answer_ping;
+pub mod parser;
