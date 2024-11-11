f2df5353896919c14cf3611186264d9d2a5099e9
Isak Möllersten <isak@kth.se>
2024-11-11 15:03:46 +0100

    test

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -1,5 +1,21 @@
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
+use std::collections::HashMap;
+
+pub struct HMap {
+    pub hm: HashMap<String, String>
+}
+
+impl HMap {
+    pub async fn handle_get(&mut self) {
+
+    }
+
+    pub async fn handle_set(&mut self, key: String, value: String, stream: &mut TcpStream) {
+        self.hm.insert(key, value);
+        stream.write_all(b"+OK\r\n");
+    }
+}
 
 pub async fn handle_ping(stream: &mut TcpStream) {
     stream.write_all(b"+PONG\r\n").await.unwrap();
@@ -9,3 +25,5 @@ pub async fn handle_echo(stream: &mut TcpStream, precursor: String, msg: String)
     let message: String = format!("{precursor}\r\n{msg}\r\n");
     stream.write_all(message.as_bytes()).await.unwrap();
 }
+
+
--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,14 @@
 use tokio::net::TcpStream;
 use tokio::io::AsyncReadExt;
 use crate::answer_ping::*;
+use std::collections::HashMap;
 
 pub async fn parse_bytes(mut stream: TcpStream) {
     let mut buf = [0; 512];
+    let hm: HashMap<String, String> = HashMap::new();
+    let mut mp = HMap {
+        hm,
+    }; 
     tokio::spawn(async move {
         loop {
             let read_count = stream.read(&mut buf).await.unwrap();
@@ -14,12 +19,14 @@ pub async fn parse_bytes(mut stream: TcpStream) {
 
             let lines: Vec<&str> = message.lines().collect();
 
-            if lines.len() > 1 {
+            if lines.len() > 2 {
                 match lines[2] {
                     "PING" => handle_ping(&mut stream).await,
-                    "ECHO" if lines.len() > 2 => {
+                    "ECHO" if lines.len() > 3 => {
                         handle_echo(&mut stream, lines[3].to_string(), lines[4].to_string()).await;
                     },
+                    "SET" => mp.handle_set(lines[4].to_string(), lines[5..=6].concat(), &mut stream).await,
+                    "GET" => mp.handle_get().await,
                     _ => break,
                 };
             }
