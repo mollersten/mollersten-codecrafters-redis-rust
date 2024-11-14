23383d08ea732314354bbd6f7f2f0b2d35ec96ab
Isak Möllersten <isak@kth.se>
2024-11-14 16:44:25 +0100

    codecrafters submit [skip ci]

--- src/answer_ping.rs
+++ src/answer_ping.rs
@@ -1,23 +1,63 @@
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 use std::collections::HashMap;
+use std::time::{Instant, Duration};
+
+struct MapItem {
+    value: String,
+    expiration_time: Option<String>,
+    time_set: Option<Instant>,
+}
 
 pub struct Storage {
-    pub storage: HashMap<String, String>,
+    storage: HashMap<String, MapItem>,
 }
 
 impl Storage {
-    pub async fn handle_get(&mut self, key: String, stream: &mut TcpStream) {
-        let value = match self.storage.get(&key) {
-            Some(val) => val,
-            None => &"$-1\r\n".to_string(),
-        };
-        stream.write_all(value.as_bytes()).await.unwrap();
+
+    pub async fn new() -> Self {
+        Self {
+            storage: HashMap::new(),
+        }
     }
 
-    pub async fn handle_set(&mut self, key: String, number: String, value: String, stream: &mut TcpStream) {
-        self.storage.insert(key, format!("{number}\r\n{value}\r\n"));
+    pub async fn handle_get(&mut self, key: String, stream: &mut TcpStream) {
+        let item = match self.storage.get(&key) {
+            Some(val) => {
+                match &val.expiration_time {
+                    Some(_) => {
+                        if Self::valid_get(val).await {
+                            val
+                        } else {
+                            &MapItem { value: "$-1\r\n".to_string(), expiration_time: None, time_set: None }
+                        }
+                    },
+                    None => val
+                }
+            }
+            None => &MapItem { value: "$-1\r\n".to_string(), expiration_time: None, time_set: None },
+        };
+        stream.write_all(item.value.as_bytes()).await.unwrap();
+    }
+
+    pub async fn handle_set(&mut self, key: String, number: String, value: String, stream: &mut TcpStream, px: Option<String>) {
+        self.storage.insert(key, MapItem { 
+            value: format!("{number}\r\n{value}\r\n"), 
+            expiration_time: px.clone(),
+            time_set: if px.is_some() {
+                Some(Instant::now())
+            } else {
+                None
+            }
+        });
         stream.write_all(b"+OK\r\n").await.unwrap();
+    }
+
+    async fn valid_get(item: &MapItem) -> bool {
+        let expiration_time = Duration::from_millis(item.expiration_time.clone().unwrap().parse()
+            .expect("Must have been no PX."));
+
+        item.time_set.expect("No time set...").elapsed() < expiration_time   
     }
 }
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,13 @@
 use tokio::net::TcpStream;
 use tokio::io::AsyncReadExt;
 use crate::answer_ping::*;
-use std::collections::HashMap;
 
 pub async fn parse_bytes(mut stream: TcpStream) {
     let mut buf = [0; 512];
-    let mut mp = Storage {
-        storage: HashMap::new(),
-    }; 
+    let mut mp = Storage::new().await;
     tokio::spawn(async move {
         loop {
+            
             let read_count = stream.read(&mut buf).await.unwrap();
             if read_count == 0 {
                 break;
@@ -23,8 +21,18 @@ pub async fn parse_bytes(mut stream: TcpStream) {
                     "PING" => handle_ping(&mut stream).await,
                     "ECHO" if lines.len() > 3 => {
                         handle_echo(&mut stream, lines[3].to_string(), lines[4].to_string()).await;
+                       
                     },
-                    "SET" => mp.handle_set(lines[4].to_string(), lines[5].to_string(), lines[6].to_string(), &mut stream).await,
+                    "SET" => {
+                        let str_px: Option<String> = lines.get(8).map(|str| str.to_string());
+                        let mut px: Option<String> = None;
+                        if str_px.is_some() && str_px.unwrap().eq("px") {
+                            px = lines.get(10).map(|x| x.to_string());
+                        }
+                        mp.handle_set(lines[4].to_string(), 
+                            lines[5].to_string(), 
+                            lines[6].to_string(), &mut stream, px).await;
+                    },
 
                     "GET" => mp.handle_get(lines[4].to_string(), &mut stream).await,
                     c => panic!("Can't handle command {c}!"),
